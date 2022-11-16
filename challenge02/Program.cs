bf315eff01dbe36b35c390cfd4bde3d196ee9f67
Luis Miguel Arpón <lmaimr@hotmail.com>
2022-11-16 15:41:05 +0000

    Segundo reto terminado

---
+++ challenge02/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string textoEncriptado = File.ReadAllText("encrypted.txt");
+Console.WriteLine(textoEncriptado);
+
+string[] palabrasEncriptadas = textoEncriptado.Split(" ");
+List<string> palabras = new ();
+
+foreach (string palabraEncriptada in palabrasEncriptadas) {
+    int posicion = 0;
+    string palabra = "";
+    while (posicion < palabraEncriptada.Length) {
+        string ascii = "";
+        if (palabraEncriptada.Substring(posicion, 1) == "9") {
+            ascii = palabraEncriptada.Substring(posicion, 2);
+            posicion += 2;
+        } else {
+            ascii = palabraEncriptada.Substring(posicion, 3);
+            posicion += 3;
+        }
+        palabra += (char)int.Parse(ascii);
+    }
+    palabras.Add(palabra);
+}
+
+Console.WriteLine(string.Join(" ", palabras));
